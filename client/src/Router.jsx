a41406cb53a0ff1ceab9e5784f521352570da0f9
Miguel Nieva <m@mikenieva.com>
2023-11-25 09:56:06 -0600

    Ruteo

---
+++ client/src/Router.jsx
@@ -0,0 +1,73 @@
+// ./src/Router.jsx
+
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+function Router() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <p>Este es el home</p>
+              </>
+            }
+          />
+
+          <Route
+            path="/iniciar-sesion"
+            element={
+              <>
+                <p>Está página es iniciar sesión</p>
+              </>
+            }
+          />
+          <Route
+            path="/registro"
+            element={
+              <>
+                <p>Está página es registro de usuario</p>
+              </>
+            }
+          />
+          <Route
+            path="/perfil"
+            element={
+              <>
+                <p>Está página es mi perfil de usuario</p>
+              </>
+            }
+          />
+          <Route
+            path="/carrito"
+            element={
+              <>
+                <p>Está página es el carrito de compras</p>
+              </>
+            }
+          />
+          <Route
+            path="/pizzas"
+            element={
+              <>
+                <p>Este es el listado de pizzas</p>
+              </>
+            }
+          />
+          <Route
+            path="/pizzas/:slug"
+            element={
+              <>
+                <p>Esta es una pizza individual</p>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default Router
