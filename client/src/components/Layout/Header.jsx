3aed13e0884009b5566e8f23aca9bfab6f538bd3
Mike Nieva <m@mikenieva.com>
2023-12-02 12:09:29 -0600

    Barras de navegación

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,7 +1,22 @@
 // ./src/components/Layout/Header.jsx
+import { Link } from "react-router-dom"
 
 function Header() {
-  return <div>Header</div>
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/registro">Registro</Link>
+        </li>
+        <li>
+          <Link to="/iniciar-sesion">Iniciar sesión</Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Header
--- client/src/routes/Auth.jsx
+++ client/src/routes/Auth.jsx
@@ -1,9 +1,10 @@
 // ./src/routes/Auth.jsx
 // Este archivos nos servirá para confirmar si el usuario está autenticado o no
 
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Navigate } from "react-router-dom"
 import UserContext from "../context/User/UserContext"
+import { Blocks } from "react-loader-spinner"
 
 // HOCs
 // High Order Components
@@ -11,6 +12,8 @@ import UserContext from "../context/User/UserContext"
 
 // eslint-disable-next-line react/prop-types
 const AuthRoute = ({ component: Component }) => {
+  const [isLoading, setIsLoading] = useState(true)
+
   // 1. OBTENER LOS DATOS DE USUARIO
   // SABER SI EL USUARIO ESTÁ AUTENTICADO O NO, CON AUTHSTATUS
   const userCtx = useContext(UserContext)
@@ -19,12 +22,35 @@ const AuthRoute = ({ component: Component }) => {
   // 2. HOOK DE EFECTO
   // VERIFICARÁ DE INICIO Y CUANDO HAYA UN CAMBIO EN AUTHSTATUS SI EL USUARIO TUVO MODIFICACIONES EN SU SESIÓN, Y SI SÍ, EJECUTARÁ EL VERIFYINGT TOKEN
   useEffect(() => {
-    verifyingToken()
+    const verifyToken = async () => {
+      await verifyingToken()
+
+      setIsLoading(false)
+    }
+
+    verifyToken()
   }, [authStatus])
 
+  console.log(isLoading)
   // 3. GENERACIÓN DE COMPONENTE
-
-  return <>{authStatus ? <Navigate replace to="/" /> : <Component />}</>
+  return (
+    <>
+      {isLoading ? (
+        <Blocks
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+        />
+      ) : authStatus ? (
+        <Navigate replace to="/" />
+      ) : (
+        <Component />
+      )}
+    </>
+  )
 }
 
 export default AuthRoute
