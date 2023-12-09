93c34044ce2e0e39ed4f1a6fbefee497d2442c91
Mike Nieva <m@mikenieva.com>
2023-12-09 09:00:59 -0600

    Remove implementation of react-use-cart

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -15,52 +15,42 @@ import Private from "./routes/Private"
 import Home from "./pages/home"
 import Cart from "./pages/carrito"
 
-import { CartProvider } from "react-use-cart"
-
 function Router() {
   return (
     <>
-      <CartProvider>
-        <UserState>
-          <PizzaState>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Layout />}>
-                  <Route index element={<Public component={Home} />} />
-                  <Route
-                    path="/iniciar-sesion"
-                    element={<Auth component={Login} />}
-                  />
-                  <Route
-                    path="/registro"
-                    element={<Auth component={SignUp} />}
-                  />
-                  <Route
-                    path="/perfil"
-                    element={
-                      <>
-                        <p>Está página es mi perfil de usuario</p>
-                      </>
-                    }
-                  />
-                  <Route
-                    path="/carrito"
-                    element={<Private component={Cart} />}
-                  />
-                  <Route
-                    path="/pizzas"
-                    element={<Public component={PizzasPage} />}
-                  />
-                  <Route
-                    path="/pizzas/:slug"
-                    element={<Public component={PizzaPage} />}
-                  />
-                </Route>
-              </Routes>
-            </BrowserRouter>
-          </PizzaState>
-        </UserState>
-      </CartProvider>
+      <UserState>
+        <PizzaState>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Public component={Home} />} />
+                <Route
+                  path="/iniciar-sesion"
+                  element={<Auth component={Login} />}
+                />
+                <Route path="/registro" element={<Auth component={SignUp} />} />
+                <Route
+                  path="/perfil"
+                  element={
+                    <>
+                      <p>Está página es mi perfil de usuario</p>
+                    </>
+                  }
+                />
+                <Route path="/carrito" element={<Private component={Cart} />} />
+                <Route
+                  path="/pizzas"
+                  element={<Public component={PizzasPage} />}
+                />
+                <Route
+                  path="/pizzas/:slug"
+                  element={<Public component={PizzaPage} />}
+                />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </PizzaState>
+      </UserState>
     </>
   )
 }
